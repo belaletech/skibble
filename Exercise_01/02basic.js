6e0b9370a6f9e88c1a5597c53acc31e10e5d04b7
belaletech <belalahmadswnbhr@gmail.com>
2023-03-26 01:25:59 +0530

    update

---
+++ Exercise_01/02basic.js
@@ -0,0 +1,18 @@
+const uid = "abc123";
+var fullName = "Belal Ahmad";
+var email = "belal@gmail.com";
+var password = "12345";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+console.log(uid);
+console.log("Full name is : ", fullName);
+console.log(email);
+
+console.log(`
+    with unique ID: ${uid}
+    user is : ${fullName}
+    and his email is : ${email}
+    and uses the password : ${password}
+ 
+ `);
