7ebb42634de90c161360c3ffa5648a6c770995ff
belaletech <belalahmadswnbhr@gmail.com>
2023-03-26 01:25:41 +0530

    update

---
+++ Exercise_01/01basic.js
@@ -0,0 +1,14 @@
+console.log("This side belal");
+var fullName = "Belal Ahmad";
+var courseName = "java Script";
+
+var isLoggedIn = false;
+var loggedCount = 34;
+
+console.log(loggedCount);
+console.log("loggedCount");
+
+var paymentMode;
+
+paymentMode = "Credit Card";
+console.log(paymentMode);
