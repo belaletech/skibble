75443e4737669919e9391aac7156d92cd5a20db5
belaletech <belalahmadswnbhr@gmail.com>
2023-03-26 16:22:02 +0530

    update

---
+++ Exercise_01/Ternary.js
@@ -0,0 +1,4 @@
+let num1 = prompt("Enter your first number");
+let num2 = prompt("Enter your second number");
+sum = num1 + num2;
+console.log("sum of two number is=", sum);
---
+++ Exercise_01/ifElse.js
@@ -0,0 +1,14 @@
+var tempreture;
+//TODO: Go to google and get the data
+
+tempreture = 20;
+
+if ((tempreture = 20)) {
+  console.log("It is very cold outside");
+}
+
+if (tempreture < 30) {
+  console.log("It's Moderate outside");
+} else {
+  console.log("It's really HOT outside");
+}
