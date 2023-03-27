edb2133a13b1e8a7493437366ce4e43b9f40bdf7
belaletech <belalahmadswnbhr@gmail.com>
2023-03-27 14:36:24 +0530

    update

--- Exercise_01/01basic.js
+++ Exercise_01/01basic.js
@@ -12,3 +12,8 @@ var paymentMode;
 
 paymentMode = "Credit Card";
 console.log(paymentMode);
+
+let num1 = Number(prompt("Enter your first number"));
+let num2 = Number(prompt("Enter your second number"));
+sum = num1 + num2;
+console.log("sum of two number is=", sum);
