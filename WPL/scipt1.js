144bf78de4cacb17d787272f370f05dbcfc983cd
belaletech <belalahmadswnbhr@gmail.com>
2023-04-04 23:19:25 +0530

    update

---
+++ WPL/scipt1.js
@@ -0,0 +1,8 @@
+let student = {
+	name : 'Belal Ahmad',
+	section : '11',
+	RollNo : '22SCSE2140046',
+};
+// Adding property to the object
+student.brakesType = 'Student information';
+console.log(student);
