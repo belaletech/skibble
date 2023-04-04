3040ac492587c05615d5c78df9eb625126df2730
belaletech <belalahmadswnbhr@gmail.com>
2023-04-04 23:18:50 +0530

    update

---
+++ Exercise_04/context.js
@@ -0,0 +1,20 @@
+// function sayHello()
+// {
+//   console.log("hay there how are you");
+// }
+// sayHello();
+// sayHello();
+
+// if (2 == "2") {
+//   console.log("This is true");
+// }
+
+// var myName="Belal Ahmad";
+// if(myName===myName){
+//   console.log("This is again a true statement");
+// }
+
+const a = '18';
+if (a == 18) console.log("you are adult");
+
+if (a === '18') console.log("You are not adult");
