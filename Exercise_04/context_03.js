384130f9f00c3af652910ad2f48c929ccfe442ba
belaletech <belalahmadswnbhr@gmail.com>
2023-04-04 23:18:38 +0530

    update

---
+++ Exercise_04/context_03.js
@@ -0,0 +1,19 @@
+// function tipper(a){
+//   var bill=a;
+//   console.log(bill+5);
+// }
+// tipper(95);
+
+function tipper(a)
+{
+  var bill=Number(a);
+  console.log(bill+5);
+}
+tipper(80);
+
+belal("255");
+var belal=function(a)
+{
+  var bill=parseInt(a);
+  console.log(bill+15);
+}
